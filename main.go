d09c2c56d437e1fa7dafa96a3c6f6315c369ab61
Jean-André Santoni <jean.andre.santoni@gmail.com>
2017-12-26 23:59:43 +0700

    Default value for c.Display

--- main.go
+++ main.go
@@ -158,6 +158,7 @@ func ChartInfoHandler(w http.ResponseWriter, r *http.Request) {
 	c.Year = 1970
 	c.Month = 1
 	c.Day = 1
+	c.Display = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23"
 	display := make([]int, C.SE_NPLANETS)
 	for i := 0; i < len(display); i++ {
 		display[i] = i
